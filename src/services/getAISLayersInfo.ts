d755766d6847d2ab523f2109c83cbed2e87fad6a
Jinnan Zhang <vannizhang@gmail.com>
2024-11-22 09:01:49 -0800

    feat: add new Vessel Traffic layers for April, May, and June 2024

--- src/services/getAISLayersInfo.ts
+++ src/services/getAISLayersInfo.ts
@@ -1267,7 +1267,107 @@ export const shipTrafficLayersData: ShipTrafficLayerInfo[] = [{
   Date: "2024_03",
   Month: 3,
   Year: 2024
-}]
+},
+{
+  /**
+   * Item Id of the Vessel Traffic Vector Tile Layer for this month
+   */
+  ArcGIS_Online_Item_ID: "a1733d808ce04a51a183e762e8b1a8c9",
+  /**
+  * Name of the Vessel Traffic Vector Tile Layer
+  */
+  Layer_Name: "US_Vessel_Traffic_2024_04_optimized",
+  /**
+   * URL of the Vessel Traffic Vector Tile Layer
+   */
+  Service_URL: "https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/US_Vessel_Traffic_2024_04_optimized/VectorTileServer",
+  /**
+   * Item id of the Feature Service (with embedded credential) that contains the polylines from the raw AIS data
+   */
+  FC_item_id: "feb9a72fc29a49b7a667219b73c1fd0b",
+  /**
+   * Name of the Feature Service
+   */
+  FC_Name: "US_Vessel_Traffic_2024_04_creds",
+  /**
+   * URL of the Feature Service (with embedded credential) that contains the polylines from the raw AIS data
+   */
+  Feature_Service: "https://utility.arcgis.com/usrsvcs/servers/feb9a72fc29a49b7a667219b73c1fd0b/rest/services/AIS/2024_US_Vessel_Traffic/FeatureServer/3",
+  Layer: "US Vessel Traffic 2024 04",
+  /**
+   * Year and Month
+   */
+  Date: "2024_04",
+  Month: 4,
+  Year: 2024
+},
+{
+  /**
+   * Item Id of the Vessel Traffic Vector Tile Layer for this month
+   */
+  ArcGIS_Online_Item_ID: "a2c60a84e2644a9e889c50a26d659078",
+  /**
+  * Name of the Vessel Traffic Vector Tile Layer
+  */
+  Layer_Name: "US_Vessel_Traffic_2024_05_optimized",
+  /**
+   * URL of the Vessel Traffic Vector Tile Layer
+   */
+  Service_URL: "https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/US_Vessel_Traffic_2024_05_optimized/VectorTileServer",
+  /**
+   * Item id of the Feature Service (with embedded credential) that contains the polylines from the raw AIS data
+   */
+  FC_item_id: "38cee93f775d44709c5f3bfe630a2686",
+  /**
+   * Name of the Feature Service
+   */
+  FC_Name: "US_Vessel_Traffic_2024_05_creds",
+  /**
+   * URL of the Feature Service (with embedded credential) that contains the polylines from the raw AIS data
+   */
+  Feature_Service: "https://utility.arcgis.com/usrsvcs/servers/38cee93f775d44709c5f3bfe630a2686/rest/services/AIS/2024_US_Vessel_Traffic/FeatureServer/4",
+  Layer: "US Vessel Traffic 2024 05",
+  /**
+   * Year and Month
+   */
+  Date: "2024_05",
+  Month: 5,
+  Year: 2024
+},
+{
+  /**
+   * Item Id of the Vessel Traffic Vector Tile Layer for this month
+   */
+  ArcGIS_Online_Item_ID: "5e5146dd37744272b2578c4910fa8508",
+  /**
+  * Name of the Vessel Traffic Vector Tile Layer
+  */
+  Layer_Name: "US_Vessel_Traffic_2024_06_optimized",
+  /**
+   * URL of the Vessel Traffic Vector Tile Layer
+   */
+  Service_URL: "https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/US_Vessel_Traffic_2024_06_optimized/VectorTileServer",
+  /**
+   * Item id of the Feature Service (with embedded credential) that contains the polylines from the raw AIS data
+   */
+  FC_item_id: "400f6c41029c44d1b2c74705631e352a",
+  /**
+   * Name of the Feature Service
+   */
+  FC_Name: "US_Vessel_Traffic_2024_06_creds",
+  /**
+   * URL of the Feature Service (with embedded credential) that contains the polylines from the raw AIS data
+   */
+  Feature_Service: "https://utility.arcgis.com/usrsvcs/servers/400f6c41029c44d1b2c74705631e352a/rest/services/AIS/2024_US_Vessel_Traffic/FeatureServer/5",
+  Layer: "US Vessel Traffic 2024 06",
+  /**
+   * Year and Month
+   */
+  Date: "2024_06",
+  Month: 6,
+  Year: 2024
+}
+]
 
 // const AIS_APP_SOURCE_DATA_URL_PROD = 'https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/AIS_App_Source_Data/FeatureServer/0';
 // const AIS_APP_SOURCE_DATA_URL_DEV = 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/AIS_App_Source_Data_DEV/FeatureServer/6/';
